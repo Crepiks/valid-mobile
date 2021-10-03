e9214d3263b3e57c5370df2970275b4a364a4121
Crepiks <sayazhan.onlassyn@mail.ru>
2021-10-03 21:44:25 +0600

    init: initialize project

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const App());
+}
+
+class App extends StatelessWidget {
+  const App({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: const Scaffold(
+            body: Center(
+          child: Text("Hello, Valid!"),
+        )));
+  }
+}
